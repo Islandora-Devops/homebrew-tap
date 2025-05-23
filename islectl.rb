316595e6cb55ca7c62bbd7466d8c8ec95ca0550b
islandora-community <community@islandora.ca>
2025-05-23 18:45:35 -0400

    Brew formula update for islectl version 0.9.13

--- islectl.rb
+++ islectl.rb
@@ -5,20 +5,20 @@
 class Islectl < Formula
   desc ""
   homepage "https://github.com/islandora-devops/islectl"
-  version "0.9.12"
+  version "0.9.13"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/Islandora-Devops/islectl/releases/download/0.9.12/islectl_Darwin_x86_64.tar.gz"
-      sha256 "99c9d06aca19d4319891aa2da4e45c66fffb9eca4c4254f081546b2e92e35dbe"
+      url "https://github.com/Islandora-Devops/islectl/releases/download/0.9.13/islectl_Darwin_x86_64.tar.gz"
+      sha256 "9033c08aaefe9c5c39801835c3a3874c5c69e76ebe0c70e206c725d70b8b719a"
 
       def install
         bin.install "islectl"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/Islandora-Devops/islectl/releases/download/0.9.12/islectl_Darwin_arm64.tar.gz"
-      sha256 "05d88b4c78fb369417e9b67c66d658e5d46fe7597f4a0641805a7d961b43fe63"
+      url "https://github.com/Islandora-Devops/islectl/releases/download/0.9.13/islectl_Darwin_arm64.tar.gz"
+      sha256 "2f1129735e276d0f08fd57468d2b9e8e799e3a3319adb01e7df0ff1337c1e799"
 
       def install
         bin.install "islectl"
@@ -28,15 +28,15 @@ class Islectl < Formula
 
   on_linux do
     if Hardware::CPU.intel? and Hardware::CPU.is_64_bit?
-      url "https://github.com/Islandora-Devops/islectl/releases/download/0.9.12/islectl_Linux_x86_64.tar.gz"
-      sha256 "f008d62500512fd5eaf4a537d66c9de4af5f61c39383f6a832d3881982e8b671"
+      url "https://github.com/Islandora-Devops/islectl/releases/download/0.9.13/islectl_Linux_x86_64.tar.gz"
+      sha256 "154fa170c6855d5b9b4b570edf708ceb7765a139dfc2905457cfdbe145543f69"
       def install
         bin.install "islectl"
       end
     end
     if Hardware::CPU.arm? and Hardware::CPU.is_64_bit?
-      url "https://github.com/Islandora-Devops/islectl/releases/download/0.9.12/islectl_Linux_arm64.tar.gz"
-      sha256 "aaff56d8e3813cc584e97b1cef6cdb121c2ec2bbfdcc79ea0df81c68ed3a9e66"
+      url "https://github.com/Islandora-Devops/islectl/releases/download/0.9.13/islectl_Linux_arm64.tar.gz"
+      sha256 "69a9890e5fce2ba4956d347789f991b2f695ae23482213d430a64888a6f3f05d"
       def install
         bin.install "islectl"
       end
